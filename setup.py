90e120155ce6fc46bffff1bfcffa3d432e756ebf
Saeyoon Oh <greentea@Saeyoons-MacBook-Air.local>
2023-04-26 11:08:21 +0900

    add example model_log dir

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read_reqs(name):
 
 setup(
     name="tvault",
-    version="0.3.15",
+    version="0.3.16",
     description="Log and find diffs between pytorch models",
     author="Saeyoon Oh",
     author_email="david@vessl.ai",
